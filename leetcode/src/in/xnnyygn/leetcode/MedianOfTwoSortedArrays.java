5d32aa4e4d4a48ea9ec240262f992d6b37353c86
xnnyygn <xnnyygn@gmail.com>
2015-03-06 20:25:14 +0800

    try median

--- leetcode/src/in/xnnyygn/leetcode/MedianOfTwoSortedArrays.java
+++ leetcode/src/in/xnnyygn/leetcode/MedianOfTwoSortedArrays.java
@@ -1,34 +1,39 @@
 package in.xnnyygn.leetcode;
 
 public class MedianOfTwoSortedArrays {
-  public double findMedianSortedArrays(int A[], int B[]) {
-    int lengthA = A.length;
-    int lengthB = B.length;
-    if (lengthA == 0) {
-      if (lengthB == 0)
-        return -1;
-      return medianOf(B, 0, lengthB - 1);
-    } else {
-      if (lengthB == 0)
-        return medianOf(A, 0, lengthA - 1);
-      return findMedianSortedArrays(A, 0, lengthA - 1, B, 0, lengthB - 1);
+
+  public double findMedianSortedArrays(int a[], int b[]) {
+    return findMedianSortedArrays(a, 0, a.length - 1, b, 0, b.length - 1);
+  }
+
+  private double findMedianSortedArrays(int a[], int leftA, int rightA, int b[], int leftB,
+      int rightB) {
+    int lenA = rightA - leftA + 1;
+    int lenB = rightB - leftB + 1;
+    if (lenA == 0) {
+      if (lenB == 0) {
+        return 0; // should not occur
+      }
+      return medianOfArray(b, leftB, rightB);
     }
+    if (lenB == 0) {
+      return medianOfArray(a, leftA, rightA);
+    }
+
+    int medianAIndex = (leftA + rightA) / 2;
+    int medianBIndex = (leftB + rightB) / 2;
+    int n = lenA <= lenB ? medianAIndex - leftA : rightB - medianBIndex;
+    if (a[medianAIndex] <= b[medianBIndex]) {
+      return findMedianSortedArrays(a, leftA + n, rightA, b, leftB, rightB - n);
+    }
+    return findMedianSortedArrays(a, leftA, rightA - n, b, leftB + n, rightB);
   }
 
-  private double medianOf(int[] array, int left, int right) {
-    if ((left - right) % 2 == 1)
-      return array[(left + right) / 2 + 1];
-    int middle = (left + right) / 2;
-    return (array[middle] + array[middle + 1]) * 1.0 / 2;
+  private double medianOfArray(int[] array, int left, int right) {
+    int len = right - left;
+    if (len % 2 != 0)
+      return array[len / 2];
+    return (array[len / 2] + array[len / 2 + 1]) * 0.5d;
   }
 
-  private double findMedianSortedArrays(int[] a, int al, int ar, int[] b, int bl, int br) {
-    // int am = (al + ar) / 2, bm = (bl + ar) / 2;
-    // int m1 = a[am], m2 = b[bm];
-    // if (m1 == m2)
-    // return m1;
-    // if (m1 < m2) findMedianSortedArrays(a, al, ar, b, bl, br)
-    // }
-    return 0;
-  }
 }
